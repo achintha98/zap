bbc50465f28532ef853dae42d7caa01b3ca39beb
achintha98 <kalanaachintha@gmail.com>
2024-12-29 19:21:29 +0530

    Implemented create zapRun and create zapRunOutBox

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -3,21 +3,38 @@ import { PrismaClient } from "@prisma/client";
 
 
 const app = express();
-const client = new PrismaClient(); 
+const client = new PrismaClient();
+app.use(express.json()); 
 
-app.post("hooks/zap/:userId/:zapId", (req, res) => {
+app.post("/hooks/zap/:userId/:zapId", (req, res) => {
+  console.log("reached 1")
+
     const userId = req.params.userId;
     const zapId = req.params.zapId
+    console.log("reached 4")
+
+    const body = req.body;
+    console.log("reached 2")
+
 
     client.$transaction(async (tx) => {
         const run = await tx.zapRun.create({
           data: {
-            zapId: zapId 
+            zapId: zapId,
+            metaData: body
           }})
+
+          console.log(run)
 
         await tx.zapRunOutBox.create({
           data: {
-            zapRunId: run.zapId 
+            zapRunId: run.id 
           }})
         })
+
+        res.json({
+            Message: "Success"
+        })
 })
+
+app.listen(3000);
