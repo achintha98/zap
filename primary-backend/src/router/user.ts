5b27619b98b2c64d0e20328d28c1fb3fca077c7f
achintha98 <kalanaachintha@gmail.com>
2025-02-15 16:01:55 +0530

    Added create user  functionality

--- primary-backend/src/router/user.ts
+++ primary-backend/src/router/user.ts
@@ -1,19 +1,28 @@
 import { Router } from "express";
 import { signUpSchema, signInchema } from "../validationSchema";
+import { PrismaClient } from "@prisma/client";
 
 const userRouter = Router();
+const prisma = new PrismaClient();
 
-userRouter.post("/user", (req, res, next) => {
+userRouter.post("/user", async (req, res, next) => {
     try {
         console.log( req.body)
 
         const validatedData = signUpSchema.parse(req.body);
-        res.json({ message: "Signup successful", data: validatedData });
+        console.log(validatedData);
+        const newUser = await prisma.user.create({
+            data: {
+                name: validatedData.username,
+                email: validatedData.email,
+                Password: validatedData.password
+              },
+            })
+        res.json({ message: "Signup successful", newUser });
         } catch (error) {
         console.log(error)
         res.status(411).json({Message: "Invalid data"});
     }
 })
-
 
 export default userRouter;
--- primary-backend/src/validationSchema/index.ts
+++ primary-backend/src/validationSchema/index.ts
@@ -2,6 +2,7 @@ import {z} from "zod";
 
 const signUpSchema = z.object({
     username: z.string().min(5),
+    email: z.string().email(),
     password: z.string().min(5),
   });
   
