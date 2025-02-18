37b8538a5ee68eea2c6d8af5a21646430fd3f490
achintha98 <kalanaachintha@gmail.com>
2025-02-18 20:41:06 +0530

    Added User model

---
+++ primary-backend/src/models/User.ts
@@ -0,0 +1,6 @@
+export interface User {
+    id: string; // UUID or ObjectId (depends on your DB)
+    name:     String
+    email:    String
+    Password: String
+}
--- primary-backend/src/modules/middleware.ts
+++ primary-backend/src/modules/middleware.ts
@@ -1,14 +1,36 @@
 import { NextFunction, Request, Response } from "express";
-import express from "express"
-import * as bcrypt from "bcrypt";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import { User } from "../models/User";
 
+dotenv.config();
 
+const SECRET = process.env.SECRET;
 
-export function authMiddleWare(req: Request, res: Response, next: NextFunction) {
+interface UserRequest extends Request {
+    user: User;
+}
+
+export function authMiddleWare(req: UserRequest, res: Response, next: NextFunction) {
     const authHeader = req.headers.authorization;
     if(!authHeader) {
         return res.status(403).json({ message: 'No token provided' });
     }
-    const user = authHeader.split(" ")
+    const jwtString = authHeader.split(" ")
+    if(jwtString) {
+        return res.status(403).json({ message: 'No token provided' });
+    }
+    if (!SECRET) {
+        throw new Error('SECRET_KEY is not defined in environment variables');
+    }
+    try {
+        const decodedUser = jwt.verify(jwtString, SECRET);
+        req.user = decodedUser as User;
+
+    }
+    catch(error) {
+        return res.status(403).json({ error: "Forbidden: Invalid or expired token" });
+    }
+
     next();
 }
